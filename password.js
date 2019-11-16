91d0922591e93a23a45a4d72d00cb681a22b9008
james hamilton <jhamilton72.jh@gmail.com>
2019-11-16 10:01:34 -0700

    changes to my css

--- password.js
+++ password.js
@@ -57,3 +57,13 @@ function myFunction(){
 
 
 myFunction();
+function copyfunction() {
+    /* Get the text field */
+    var copyText = document.getElementById("password");
+    /* Select the text field */
+    copyText.select();
+    copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+    /* Copy the text inside the text field */
+    document.execCommand("copy");
+  }
+  
